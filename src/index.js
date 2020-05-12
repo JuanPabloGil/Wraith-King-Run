0dd78fa211a364e040048d98e0f04da8f5b90175
Juan Pablo <jpablomgil@gmail.com>
2020-05-12 13:38:29 -0500

    Create set default values to game options and config of the game

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import Phaser from "phaser";
+import Bootloader from "./bootloader";
+import PlayGame from './scenes/playGame';
+
+const gameOptions = {
+  playerGravity: 900,
+  playerGrip: 100,
+  playerSpeed: 200,
+  playerJump: 400,
+  playerDoubleJump: 300
+};
 
 const config = {
   type: Phaser.CANVAS,
   parent: "container",
-  width: 600,
+  width: 800,
   height: 600,
   backgroundColor: '#2d2d2d',
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 300
+      }
+    }
+  },
   scene: [
-    Bootloader
+    Bootloader,
+    PlayGame
   ]
+
 };
+
 
 new Phaser.Game(config);
